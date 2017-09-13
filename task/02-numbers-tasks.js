385b9856f9938c392c20186bdd591c9806d6ae08
naga_pulivarthy <naga_pulivarthy@epam.com>
2017-09-13 19:46:25 +0530

    Task 02 Numbers

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -54,7 +54,7 @@ function getCicleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(value1, value2) {
-    return ((value1 /2)  + (value2 / 2));
+    return ((value1 / 2) + (value2 / 2));
 }
 
 /**
@@ -92,7 +92,7 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   5*x = 0         => 0
  */
 function getLinearEquationRoot(a, b) {
-    throw new Error('Not implemented');
+    return -b / a;
 }
 
 
@@ -114,7 +114,7 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (1,2)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-    throw new Error('Not implemented');
+    return Math.abs(Math.atan2(y2, x2) - Math.atan2(y1, x1));
 }
 
 /**
@@ -130,7 +130,7 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     0     => 0
  */
 function getLastDigit(value) {
-    throw new Error('Not implemented');
+    return value % 10;
 }
 
 
@@ -146,7 +146,7 @@ function getLastDigit(value) {
  * '-525.5'     => -525.5
  */
 function parseNumberFromString(value) {
-    throw new Error('Not implemented');
+    return parseInt(value);
 }
 
 /**
@@ -162,8 +162,8 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelipidedDiagonal(a,b,c) {
-    throw new Error('Not implemented');
+function getParallelipidedDiagonal(a, b, c) {
+    return Math.hypot(a, b, c);
 }
 
 /**
@@ -184,7 +184,8 @@ function getParallelipidedDiagonal(a,b,c) {
  *   1678, 3  => 2000
  */
 function roundToPowerOfTen(num, pow) {
-    throw new Error('Not implemented');
+    var powOfTen = Math.pow(10, pow);
+    return Math.round(num / powOfTen) * powOfTen;
 }
 
 /**
@@ -205,7 +206,17 @@ function roundToPowerOfTen(num, pow) {
  *   17 => true
  */
 function isPrime(n) {
-    throw new Error('Not implemented');
+    var i;
+    if (n < 4) {
+        return true;
+    }
+    for (i = 2; i <= Math.sqrt(n) ; i++) {
+        if (((n % i) === 0)) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 /**
