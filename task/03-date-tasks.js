9a18531290420df1d2a5ccc990ec3c3c75a389aa
unknown <naga09sindhura@gmail.com>
2017-09-16 20:28:50 +0530

    parseDataFromRfc2822

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,9 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   console.log(new Date(value));
-   console.log(Date.parse(value));
-   return new Date(value);
+   return Date.parse(value);
 }
 
 /**
